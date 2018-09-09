4e4060117fb38137bf3e45755b14fa9895f1e8b0
Wangweizhen <hawking.rei@gmail.com>
2018-09-09 14:20:11 +0800

    feat: add fish

---
+++ fish/config.fish
@@ -0,0 +1,3 @@
+set -gx PATH /usr/local/opt/ansible@2.0/bin /usr/local/opt/icu4c/bin /usr/local/opt/icu4c/sbin $PATH
+set -gx PATH /usr/bin /bin /usr/sbin /sbin /Users/loveknut/.cargo/bin /Users/loveknut/go/bin $PATH
+alias H "env http_proxy=http://127.0.0.1:8123 https_proxy=http://127.0.0.1:8123"
